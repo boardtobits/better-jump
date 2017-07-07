c201a4ad4cba430e74a658912ff174efac002174
boardtobits <boardtobits@gmail.com>
2017-07-07 15:28:32 -0400

    Added files via upload.

---
+++ BetterJump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BetterJump : MonoBehaviour {
+
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+
+	Rigidbody2D rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+		if (rb.velocity.y < 0) {
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		} else if (rb.velocity.y > 0 && !Input.GetButton ("Jump")) {
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+	}
+
+}
---
+++ Jump.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	[Range(1, 10)]
+	public float jumpVelocity;
+
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;	
+		}
+	}
+}
